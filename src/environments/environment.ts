64170e4ba06b45c08b7b2cb8cc264f59a09adbbd
VU AN KHANH <vuankhanh19071992@gmail.com>
2024-09-26 00:00:11 +0700

    Refactor environment.ts: Update backend API and static URLs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  backendApi: 'http://192.168.1.2:3004/api',
-  backendStatic: 'http://192.168.1.2:3004/static'
+  backendApi: 'http://192.168.1.2:8181/api',
+  backendStatic: 'http://192.168.1.2:8181/static'
 };
