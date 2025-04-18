6e258e009800657ed45cd462d44f49570059ec79
VU AN KHANH <vuankhanh19071992@gmail.com>
2025-04-19 01:01:27 +0700

    feat: Refactor address selector component to use autocomplete for province, district, and ward inputs

--- src/app/shared/component/address-selector/address-selector.component.ts
+++ src/app/shared/component/address-selector/address-selector.component.ts
@@ -1,15 +1,20 @@
-import { Component, forwardRef, inject, OnDestroy, OnInit } from '@angular/core';
-import { ControlValueAccessor, FormBuilder, FormGroup, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
+import { AfterViewInit, Component, ElementRef, forwardRef, inject, OnDestroy, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { ControlValueAccessor, FormBuilder, FormGroup, FormsModule, NG_VALUE_ACCESSOR, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MaterialModule } from '../../module/material';
 import { VnPublicService } from '../../service/api/vn-public.service';
-import { Observable, of, Subscription } from 'rxjs';
+import { BehaviorSubject, map, Observable, of, startWith, Subscription, switchMap } from 'rxjs';
 import { IProvince } from '../../interface/vn-public-apis.interface';
 import { MatSelectChange } from '@angular/material/select';
+import { MatInput } from '@angular/material/input';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-address-selector',
   standalone: true,
   imports: [
+    CommonModule,
+    FormsModule,
     ReactiveFormsModule,
     MaterialModule
   ],
@@ -23,27 +28,54 @@ import { MatSelectChange } from '@angular/material/select';
   templateUrl: './address-selector.component.html',
   styleUrl: './address-selector.component.scss'
 })
-export class AddressSelectorComponent implements ControlValueAccessor, OnInit, OnDestroy {
+export class AddressSelectorComponent implements ControlValueAccessor, OnInit, AfterViewInit, OnDestroy {
+  @ViewChild('provinceEl') provinceEl!: ElementRef<MatInput>;
+  @ViewChild('districtEl') districtEl!: ElementRef<MatInput>;
+  @ViewChild('wardEl') wardEl!: ElementRef<MatInput>;
+  @ViewChild('streetEl') streetEl!: ElementRef<MatInput>;
+
+  private readonly renderer = inject(Renderer2)
+  private readonly fb = inject(FormBuilder);
   private readonly vnPublicService = inject(VnPublicService);
   addressForm: FormGroup;
   provinces$: Observable<IProvince[]> = of([]);
   districts$: Observable<IProvince[]> = of([]);
   wards$: Observable<IProvince[]> = of([]);
 
-  subscription: Subscription = new Subscription();
-  constructor(
-    private fb: FormBuilder
-  ) {
+  bProvinceInputChange = new BehaviorSubject<string>('');
+  bDistrictInputChange = new BehaviorSubject<string>('');
+  bWardInputChange = new BehaviorSubject<string>('');
+
+  private subscription: Subscription = new Subscription();
+  constructor() {
     this.addressForm = this.fb.group({
-      province: [''],
-      district: [''],
-      ward: [''],
+      province: ['', Validators.required],
+      district: ['', Validators.required],
+      ward: ['', Validators.required],
       street: [''],
     });
 
-    this.addressForm.valueChanges.subscribe(value => {
-      this.onChange(value);
-    });
+    this.subscription.add(
+      this.addressForm.valueChanges.subscribe(value => {
+        this.onChange(value);
+      })
+    )
+  }
+
+  get provinceControl() {
+    return this.addressForm.get('province')!;
+  }
+
+  get districtControl() {
+    return this.addressForm.get('district')!;
+  }
+
+  get wardControl() {
+    return this.addressForm.get('ward')!;
+  }
+
+  get streetControl() {
+    return this.addressForm.get('street');
   }
 
   onChange = (value: any) => {};
@@ -75,35 +107,92 @@ export class AddressSelectorComponent implements ControlValueAccessor, OnInit, O
     this.setProvince$();
   }
 
-  onProvinceChange(event: MatSelectChange) {
-    const province: IProvince = event.value;
-    console.log(province);
+  onProvinceOptionSelected(event: MatAutocompleteSelectedEvent) {
+    const province: IProvince = event.option.value;
+    this.provinceEl.nativeElement.value = province.name;
+    this.districtEl.nativeElement.value = null;
+    this.wardEl.nativeElement.value = null;
+
+    this.provinceControl.setValue(province);
     this.setDistrict$(province.code);
+    this.districtControl.setValue(null);
+    this.wardControl.setValue(null);
   }
 
-  onDistrictChange(event: MatSelectChange) {
-    const district: IProvince = event.value;
-    console.log(district);
+  onDistrictOptionSelected(event: MatAutocompleteSelectedEvent) {
+    const district: IProvince = event.option.value;
+    this.districtEl.nativeElement.value = district.name;
+    this.wardEl.nativeElement.value = null;
+
+    this.districtControl.setValue(district);
     this.setWard$(district.code);
+    this.wardControl.setValue(null);
   }
 
-  onWardChange(event: MatSelectChange) {
-    const ward: IProvince = event.value;
-    console.log(ward);
+  onWardOptionSelected(event: MatAutocompleteSelectedEvent) {
+    const ward: IProvince = event.option.value;
+    this.wardEl.nativeElement.value = ward.name;
+
+    this.wardControl.setValue(ward);
   }
 
   private setProvince$() {
-    this.provinces$ = this.vnPublicService.getProvinces();
+    this.provinces$ = this.vnPublicService.getProvinces().pipe(
+      switchMap((provinces: IProvince[]) => this.bProvinceInputChange.pipe(
+        startWith(''),
+        map((value: string) => {
+          const filterValue = value.toLowerCase();
+          return provinces.filter((province: IProvince) => province.name.toLowerCase().includes(filterValue));
+        })
+      ))
+    );
   }
 
   private setDistrict$(provinceCode: string) {
-    this.districts$ = this.vnPublicService.getDistricts(provinceCode);
+    this.districts$ = this.vnPublicService.getDistricts(provinceCode).pipe(
+      switchMap((districts: IProvince[]) => this.bDistrictInputChange.pipe(
+        startWith(''),
+        map((value: string) => {
+          const filterValue = value.toLowerCase();
+          return districts.filter((district: IProvince) => district.name.toLowerCase().includes(filterValue));
+        })
+      ))
+    );
   }
 
   private setWard$(districtCode: string) {
-    this.wards$ = this.vnPublicService.getWards(districtCode);
+    this.wards$ = this.vnPublicService.getWards(districtCode).pipe(
+      switchMap((wards: IProvince[]) => this.bWardInputChange.pipe(
+        startWith(''),
+        map((value: string) => {
+          const filterValue = value.toLowerCase();
+          return wards.filter((ward: IProvince) => ward.name.toLowerCase().includes(filterValue));
+        })
+      ))
+    );
   }
 
+  ngAfterViewInit(): void {
+    this.renderer.listen(this.provinceEl.nativeElement, 'input', (event: InputEvent ) => {
+      this.bProvinceInputChange.next(this.provinceEl.nativeElement.value);
+
+      this.provinceControl.setValue(null);
+      this.districtControl.setValue(null);
+      this.wardControl.setValue(null);
+    });
+  
+    this.renderer.listen(this.districtEl.nativeElement, 'input', () => {
+      this.bDistrictInputChange.next(this.districtEl.nativeElement.value);
+
+      this.districtControl.setValue(null);
+      this.wardControl.setValue(null);
+    });
+  
+    this.renderer.listen(this.wardEl.nativeElement, 'input', () => {
+      this.bWardInputChange.next(this.wardEl.nativeElement.value);
+      this.wardControl.setValue(null);
+    });
+  }
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
