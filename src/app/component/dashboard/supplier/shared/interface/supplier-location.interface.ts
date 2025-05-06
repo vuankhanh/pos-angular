758da1f5df333180ad9b564f476c1760be0f695a
VU AN KHANH <vuankhanh19071992@gmail.com>
2025-05-06 23:18:23 +0700

    feat: Add optional debt field to supplier location interface

--- src/app/component/dashboard/supplier/shared/interface/supplier-location.interface.ts
+++ src/app/component/dashboard/supplier/shared/interface/supplier-location.interface.ts
@@ -12,6 +12,7 @@ export interface ISupplierLocation {
     street: string;
   };
   telephone: string; // Contact phone number
+  debt?: ISupplierDebt; // Optional field for supplier debt
   email?: string; // Contact email
   position?: {
     lat: string | number; // Latitude for geolocation
@@ -27,6 +28,11 @@ export interface ISupplierLocation {
   };
   logo?: string; // URL to the supplierLocation's logo (optional)
   sameAs?: string[]; // URLs to social media or related profiles (optional)
+}
+
+export interface ISupplierDebt {
+  amount: number; // Amount owed to the supplier
+  note: string; // Additional notes (optional)
 }
 
 export type TSupplierLocationModel = ISupplierLocation & IMongodbDocument;
